706d8bd04bae5cc2fe0cfdcb940140282ac1da6a
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2019-01-03 13:38:07 +0100

    Strings must use singlequote.
    https://travis-ci.org/onliniak/ocean-cream/jobs/474832630

--- js/buttons.js
+++ js/buttons.js
@@ -2,24 +2,24 @@
 // And mobinu function say 'if menu is hiden and someone click, open it but if is open, hide it'
 // https://www.w3schools.com/jsref/event_onclick.asp
 
-document.getElementById("hamburger-cream").addEventListener("click", mobinu);
+document.getElementById('hamburger-cream').addEventListener('click', mobinu);
 
 function mobinu() {
-  var x = document.getElementById("mobinu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var x = document.getElementById('mobinu');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
   } else {
-    x.style.display = "block";
+    x.style.display = 'block';
   }
 }
 
-document.getElementById("ShoSid").addEventListener("click", sidiBer);
+document.getElementById('ShoSid').addEventListener('click', sidiBer);
 
 function sidiBer() {
-  var x = document.getElementById("sidiBer");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var x = document.getElementById('sidiBer');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
   } else {
-    x.style.display = "block";
+    x.style.display = 'block';
   }
 }
