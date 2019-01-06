c899e82c10f7e8325e644db01ac6730bbc71d8e8
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2019-01-06 01:03:44 +0100

    Fix parse error on PHP 7.2
    I prefer a mess than cleanup where I can not find myself - php parser

--- index.php
+++ index.php
@@ -50,12 +50,7 @@ body_class(); ?>>
   <!-- Your post -->
  <article <?php
     post_class(); ?>>     
-    <?php
-
-    if (have_posts()) :
-        while (have_posts()) {
-            : the_post();
-        } ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
      <h1><a href="<?php
         the_permalink(); ?>"><?php
         the_title(); ?></a></h1>
@@ -104,9 +99,8 @@ if (is_singular()) {
     echo '</div>';
 } elseif (is_tax()) {
     ;
-}?>           <?php
-    endwhile;
-else: ?>
+}?>          
+	 <?php endwhile; else : ?>
             <p>
               <?php
                 esc_html_e('Sorry, no posts matched your criteria.', 'ocean-cream'); ?>
