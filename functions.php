ac37ea3cf6329264d08d4526f51614f71ade3b2c
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2019-01-06 01:27:07 +0100

    Fix js error
    I prefer a mess than cleanup where I can not find myself - js parser

--- functions.php
+++ functions.php
@@ -68,8 +68,8 @@ add_action('wp_footer', 'ocean_cream_footer_styles');
 // https://stackoverflow.com/questions/4221870/how-to-put-my-javascript-in-the-footer
 // https://developers.google.com/web/tools/lighthouse/audits/blocking-resources
 function ocean_cream_footer_scripts() { ?>
-<script nonce="aHR0cHM6Ly9naXRodWIuY29tL29ubGluaWFrL29jZWFuLWNyZWFtL2VkaXQvbWFzdGVyL2Z1bmN0aW9ucy5waHA=">function mobinu(){document.onkeydown=function(e){var t=(e=e||event).which||e.keyCode;100==t&&(location="<?php echo esc_html(get_permalink(get_adjacent_post(false, '', true))) ?>"),102==t&&(location="<?php echo esc_html(get_permalink(get_adjacent_post(false, '', false))); ?>")};</script>
+<script>document.onkeydown=function(e){var t=(e=e||event).which||e.keyCode;100==t&&(location="<?php echo esc_html( get_permalink(get_adjacent_post(false,'',true))) ?>"),102==t&&(location="<?php echo esc_html( get_permalink(get_adjacent_post(false,'',false))); ?>")};</script>
 <?php
-}
-add_action('wp_footer', 'ocean_cream_footer_scripts');
+									  }
+add_action( 'wp_footer', 'ocean_cream_footer_scripts' );
 ?>
