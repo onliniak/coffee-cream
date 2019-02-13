25dd3972ce42b12269eb741ff2fb05a55327ced2
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2019-02-13 16:41:21 +0100

    Don't steal me ! ß
    Add simple text after copied content

---
+++ js/copyME.js
@@ -0,0 +1,23 @@
+ function addLink() {
+        //Get the selected text and append the extra info
+        //Steal from https://stackoverflow.com/questions/2026335/how-to-add-extra-info-to-copied-web-text
+        var selection = window.getSelection(),
+            pagelink = '<p> Read more at: ' + document.location.href,
+            copytext = selection + pagelink,
+            newdiv = document.createElement('div');
+
+        //hide the newly created container
+        newdiv.style.position = 'absolute';
+        newdiv.style.left = '-99999px';
+
+        //insert the container, fill it with the extended text, and define the new selection
+        document.body.appendChild(newdiv);
+        newdiv.innerHTML = copytext;
+        selection.selectAllChildren(newdiv);
+
+        window.setTimeout(function () {
+            document.body.removeChild(newdiv);
+        }, 100);
+    }
+
+    document.addEventListener('copy', addLink);
