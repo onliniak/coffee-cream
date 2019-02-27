2155c0ad75ff1ea66f86a6a671b0b40d63d3f8cb
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2019-02-28 00:41:52 +0100

    Update keyboard.js

--- js/keyboard.js
+++ js/keyboard.js
@@ -8,10 +8,10 @@ document.onkeydown = function (e) {
 			//numpad 4
 			//previous post
     if (keycode == 100)
-        location = "<?php echo esc_html( get_permalink(get_adjacent_post(false,'',false))) ?>";
+        location = OCreamPPost // Ocean Cream Previous Post
 			//right
 			//numpad 6
 			//next post
     if (keycode == 102)
-        location = "<?php echo esc_html( get_permalink(get_adjacent_post(false,'',true))); ?>";
+        location = OCreamNTP // Ocean Cream NexT Post
 }
