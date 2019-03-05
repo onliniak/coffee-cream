b9d4451b7482ff1850fc56309821a5b29c22565c
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2019-03-05 14:48:15 +0100

    Customizer support
    Native customizer API

--- functions.php
+++ functions.php
@@ -1,10 +1,5 @@
 <?php
-# include 'kirki_customizer.php';
-# I use kirki plugin as customizer framework, so if you want to change colors via customizer → please,
-# install https://pl.wordpress.org/plugins/kirki/ or help me with rewrite my code to WordPress customizer API.
-# After installation, please delete first # (uncomment include include 'kirki_customizer.php';)
-# No, I've tried native WP Customizer API, but it not work. 
-
+include 'inc/customizer.php';
 // Add menu in header
 function ocean_cream_menu() {
     $locations = array(
