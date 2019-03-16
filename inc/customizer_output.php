5cb8c7eed9ffeb1aed2f6c4b610c3cb12d7f95f0
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2019-03-16 14:01:41 +0100

    Fix white default background

--- inc/customizer_output.php
+++ inc/customizer_output.php
@@ -3,7 +3,8 @@ function ocean_cream_customize_css()
 {
     ?>
          <style>
-       .button{ background-color: <?php echo get_theme_mod('accent_background', '#00b0ff'); ?>; }
+		  	 body {background-color: #<?php background_color(); ?>}
+       			 .button{ background-color: <?php echo get_theme_mod('accent_background', '#00b0ff'); ?>; }
 			 .button{ color: <?php echo get_theme_mod('accent_text', '#3e2723'); ?>; }
 			 .button:hover{ background-color: <?php echo get_theme_mod('accent_hover', '#2a91d1'); ?>; }
 			 .page-numbers, .OCnextpage a{ background-color: <?php echo get_theme_mod('navigation_background', '#00b0ff'); ?>;}
