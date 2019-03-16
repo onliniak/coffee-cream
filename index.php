7c68de52410be74b6a23a360fd0601672975beed
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2019-03-16 20:23:33 +0100

    Change svg icon

--- index.php
+++ index.php
@@ -24,16 +24,11 @@ body_class(); ?>>
         'container_class' => 'menu'
         )
     ); ?>
-        </div>
-        <!-- Icons from Freepic -->
+	    </div>
 		<span id="hamburger-cream">
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="25px" height="25px" viewBox="0 0 124 124" style="enable-background:new 0 0 124 124;" xml:space="preserve">
-<g>
-    <path d="M112,6H12C5.4,6,0,11.4,0,18s5.4,12,12,12h100c6.6,0,12-5.4,12-12S118.6,6,112,6z" fill="#FFFFFF"/>
-    <path d="M112,50H12C5.4,50,0,55.4,0,62c0,6.6,5.4,12,12,12h100c6.6,0,12-5.4,12-12C124,55.4,118.6,50,112,50z" fill="#FFFFFF"/>
-    <path d="M112,94H12c-6.6,0-12,5.4-12,12s5.4,12,12,12h100c6.6,0,12-5.4,12-12S118.6,94,112,94z" fill="#FFFFFF"/>
-    </g>
-</svg>
+<svg class="i-menu" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+            <path d="M4 8 L28 8 M4 16 L28 16 M4 24 L28 24"></path>
+        </svg>
 			</span>
     </header>
 
