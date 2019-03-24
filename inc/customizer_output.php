54ce37507cd6b6c03a4b95da340b00d3ef0d443b
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2019-03-24 03:33:26 +0000

    nav.menu

--- inc/customizer_output.php
+++ inc/customizer_output.php
@@ -4,16 +4,16 @@ function ocean_cream_customize_css()
     ?>
          <style>
 			 body {background-color: #<?php background_color(); ?>}
-       .button{ background-color: <?php echo get_theme_mod('accent_background', '#00b0ff'); ?>; }
-			 .button{ color: <?php echo get_theme_mod('accent_text', '#3e2723'); ?>; }
-			 .button:hover{ background-color: <?php echo get_theme_mod('accent_hover', '#2a91d1'); ?>; }
+       .ocean_cream_button{ background-color: <?php echo get_theme_mod('accent_background', '#00b0ff'); ?>; }
+			 .ocean_cream_button{ color: <?php echo get_theme_mod('accent_text', '#3e2723'); ?>; }
+			 .ocean_cream_button:hover{ background-color: <?php echo get_theme_mod('accent_hover', '#2a91d1'); ?>; }
 			 .page-numbers, .OCnextpage a{ background-color: <?php echo get_theme_mod('navigation_background', '#00b0ff'); ?>;}
 			 article{ background-color: <?php echo get_theme_mod('article_background', '#f5eed6'); ?>!important;}
 			 article a{ color: <?php echo get_theme_mod('article_link'); ?>;}
 			 article p, .ocean_cream_info{ color: <?php echo get_theme_mod('article_text'); ?>;}
-			 div.menu{ background-color: <?php echo get_theme_mod('menu_background', '#d2bc83'); ?>; }
-			 div.menu li a{ color: <?php echo get_theme_mod('menu_text', '#000'); ?>; }
-			 div.menu li a:hover{ background-color: <?php echo get_theme_mod('menu_item_hover', '#2a91d1'); ?>; }
+			 nav.menu{ background-color: <?php echo get_theme_mod('menu_background', '#d2bc83'); ?>; }
+			 nav.menu li a{ color: <?php echo get_theme_mod('menu_text', '#000'); ?>; }
+			 nav.menu li a:hover{ background-color: <?php echo get_theme_mod('menu_item_hover', '#2a91d1'); ?>; }
 			 aside li, svg.i-menu {color: <?php echo get_theme_mod('sidebar_text', '#ffffff'); ?>;}
 			 aside a {color: <?php echo get_theme_mod('sidebar_text_link', '#ffffff'); ?>;}
          </style>
