83b1b624f55245fc11840c002c96c295832973b0
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2019-04-07 12:52:07 +0200

    Add files via upload

--- inc/customizer.php
+++ inc/customizer.php
@@ -2,197 +2,255 @@
 function ocean_cream_customize_register($wp_customize)
 {
     // Do stuff with $wp_customize, the WP_Customize_Manager object.
-    $wp_customize->add_setting('accent_background', array(
-  'type' => 'theme_mod',
-  'capability' => 'edit_theme_options',
+    $wp_customize->add_setting('ocean_cream_accent_background', array(
+        'type' => 'theme_mod',
+        'capability' => 'edit_theme_options',
         'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'accent_background',
+        'ocean_cream_accent_background',
         array(
         'label'      => esc_html__('Accent background', 'ocean-cream'),
         'description' => esc_html__('Change background color of buttons', 'ocean-cream'),
-        'settings'   => 'accent_background',
+        'settings'   => 'ocean_cream_accent_background',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('accent_text', array(
+    $wp_customize->add_setting('ocean_cream_accent_text', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
         'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'accent_text',
+        'ocean_cream_accent_text',
         array(
         'label'      => esc_html__('Accent text', 'ocean-cream'),
         'description' => esc_html__('Change text color of buttons', 'ocean-cream'),
-        'settings'   => 'accent_text',
+        'settings'   => 'ocean_cream_accent_text',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('accent_hover', array(
+    $wp_customize->add_setting('ocean_cream_accent_hover', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
         'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'accent_hover',
+        'ocean_cream_accent_hover',
         array(
         'label'      => esc_html__('Accent hover', 'ocean-cream'),
         'description' => esc_html__('Change background color of buttons, when mouse hover', 'ocean-cream'),
-        'settings'   => 'accent_hover',
+        'settings'   => 'ocean_cream_accent_hover',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('navigation_background', array(
+    $wp_customize->add_setting('ocean_cream_navigation_background', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
         'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'navigation_background',
+        'ocean_cream_navigation_background',
         array(
         'label'      => esc_html__('Pagination background', 'ocean-cream'),
         'description' => esc_html__('Change background color of navigation', 'ocean-cream'),
-        'settings'   => 'navigation_background',
+        'settings'   => 'ocean_cream_navigation_background',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('article_background', array(
+    $wp_customize->add_setting('ocean_cream_article_background', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
         'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'article_background',
+        'ocean_cream_article_background',
         array(
         'label'      => esc_html__('Post background', 'ocean-cream'),
         'description' => esc_html__('Change background color of posts (article)', 'ocean-cream'),
-        'settings'   => 'article_background',
+        'settings'   => 'ocean_cream_article_background',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('article_text', array(
+    $wp_customize->add_setting('ocean_cream_article_text', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
          'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'article_text',
+        'ocean_cream_article_text',
         array(
         'label'      => esc_html__('Post text', 'ocean-cream'),
         'description' => esc_html__('Change text color of posts (article)', 'ocean-cream'),
-        'settings'   => 'article_text',
+        'settings'   => 'ocean_cream_article_text',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('article_link', array(
+    $wp_customize->add_setting('ocean_cream_article_link', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
          'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'article_link',
+        'ocean_cream_article_link',
         array(
         'label'      => esc_html__('Post link', 'ocean-cream'),
         'description' => esc_html__('Change link color of posts (article)', 'ocean-cream'),
-        'settings'   => 'article_link',
+        'settings'   => 'ocean_cream_article_link',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('menu_background', array(
+    $wp_customize->add_setting('ocean_cream_menu_background', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
         'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'menu_background',
+        'ocean_cream_menu_background',
         array(
         'label'      => esc_html__('Menu background', 'ocean-cream'),
         'description' => esc_html__('Change background color of menu', 'ocean-cream'),
-        'settings'   => 'menu_background',
+        'settings'   => 'ocean_cream_menu_background',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('menu_text', array(
+    $wp_customize->add_setting('ocean_cream_menu_text', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
         'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'menu_text',
+        'ocean_cream_menu_text',
         array(
         'label'      => esc_html__('Menu text', 'ocean-cream'),
         'description' => esc_html__('Change text color of menu', 'ocean-cream'),
-        'settings'   => 'menu_text',
+        'settings'   => 'ocean_cream_menu_text',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('menu_item_hover', array(
+    $wp_customize->add_setting('ocean_cream_menu_item_hover', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
         'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'menu_item_hover',
+        'ocean_cream_menu_item_hover',
         array(
         'label'      => esc_html__('Menu item hover', 'ocean-cream'),
         'description' => esc_html__('Change background color of menu, when hover.', 'ocean-cream'),
-        'settings'   => 'menu_item_hover',
+        'settings'   => 'ocean_cream_menu_item_hover',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('sidebar_text', array(
+    $wp_customize->add_setting('ocean_cream_sidebar_text', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
         'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'sidebar_text',
+        'ocean_cream_sidebar_text',
         array(
         'label'      => esc_html__('Sidebar text', 'ocean-cream'),
         'description' => esc_html__('Change text color of sidebar and "Hamburger" icon color.', 'ocean-cream'),
-        'settings'   => 'sidebar_text',
+        'settings'   => 'ocean_cream_sidebar_text',
         'section' => 'colors',
     )
 ));
 
-    $wp_customize->add_setting('sidebar_text_link', array(
+    $wp_customize->add_setting('ocean_cream_sidebar_text_link', array(
   'type' => 'theme_mod',
   'capability' => 'edit_theme_options',
         'sanitize_callback' => 'sanitize_hex_color'
 ));
     $wp_customize->add_control(new WP_Customize_Color_Control(
         $wp_customize,
-        'sidebar_text_link',
+        'ocean_cream_sidebar_text_link',
         array(
         'label'      => esc_html__('Sidebar text links', 'ocean-cream'),
         'description' => esc_html__('Change color of sidebar links.', 'ocean-cream'),
-        'settings'   => 'sidebar_text_link',
+        'settings'   => 'ocean_cream_sidebar_text_link',
         'section' => 'colors',
     )
 ));
-}
+    $wp_customize->add_setting('ocean_cream_customizer_enable', array(
+  'capability' => 'edit_theme_options',
+  'default' => 'disable',
+  'sanitize_callback' => 'ocean_cream_sanitize_select',
+));
+
+    $wp_customize->add_control('ocean_cream_customizer_enable', array(
+  'type' => 'radio',
+  'section' => 'colors', // Add a default or your own section
+  'label' => esc_html__('Enable/Disable WP Customizer', 'ocean-cream'),
+  'description' => esc_html__('If disabled, changes will not be made', 'ocean-cream'),
+  'priority' => 1,
+  'choices' => array(
+    'enable' => esc_html__('Enable', 'ocean-cream'),
+    'disable' => esc_html__('Disable', 'ocean-cream'),
+  ),
+));
+    // https://divpusher.com/blog/wordpress-customizer-sanitization-examples#select
+    function ocean_cream_sanitize_select($input, $setting)
+    {
+
+  // Ensure input is a slug.
+        $input = sanitize_key($input);
+
+        // Get list of choices from the control associated with the setting.
+        $choices = $setting->manager->get_control($setting->id)->choices;
+
+        // If the input is a valid key, return it; otherwise, return the default.
+        return (array_key_exists($input, $choices) ? $input : $setting->default);
+    }
+    $wp_customize->add_setting('ocean_cream_google_fonts', array(
+  'capability' => 'edit_theme_options',
+  'sanitize_callback' => 'wp_filter_nohtml_kses', //Remove html
+));
+    $wp_customize->add_setting('ocean_cream_google_fonts_header', array(
+  'capability' => 'edit_theme_options',
+  'sanitize_callback' => 'wp_filter_nohtml_kses', //Remove html
+));
+
+    $wp_customize->add_control('ocean_cream_google_fonts', array(
+  'type' => 'text',
+  'section' => 'ocean_cream_customizer_fonts',
+  'label' => esc_html__('Google Fonts', 'ocean-cream'),
+  'description' => esc_html__('Enter the name of your favorite Google font and I will download it and prepare it for you', 'ocean-cream'),
+));
+    $wp_customize->add_control('ocean_cream_google_fonts_header', array(
+  'type' => 'text',
+  'section' => 'ocean_cream_customizer_fonts',
+  'label' => esc_html__('Google Fonts header', 'ocean-cream'),
+  'description' => esc_html__('Similar to Google Fonts, but change only headers.', 'ocean-cream'),
+));
+};
 add_action('customize_register', 'ocean_cream_customize_register');
-include 'customizer_output.php';
+function ocean_cream_customizer_fonts($wp_customize)
+{
+    $wp_customize->add_section('ocean_cream_customizer_fonts', array(
+    'title'             => __('Google Fonts', 'ocean-cream'),
+    'description'       => __('Please, enable me in colors section', 'ocean-cream'),
+));
+}
+add_action('customize_register', 'ocean_cream_customizer_fonts');
--- inc/customizer_output.php
+++ inc/customizer_output.php
@@ -1,23 +1,51 @@
 <?php
-function ocean_cream_customize_css()
-{
-    ?>
-         <style>
-			 body {background-color: #<?php background_color(); ?>}
-       .ocean_cream_button{ background-color: <?php echo get_theme_mod('accent_background', '#00b0ff'); ?>; }
-			 .ocean_cream_button{ color: <?php echo get_theme_mod('accent_text', '#3e2723'); ?>; }
-			 .ocean_cream_button:hover{ background-color: <?php echo get_theme_mod('accent_hover', '#2a91d1'); ?>; }
-			 .page-numbers, .OCnextpage a{ background-color: <?php echo get_theme_mod('navigation_background', '#00b0ff'); ?>;}
-			 article{ background-color: <?php echo get_theme_mod('article_background', '#f5eed6'); ?>!important;}
-			 article a{ color: <?php echo get_theme_mod('article_link'); ?>;}
-			 article p, .ocean_cream_info{ color: <?php echo get_theme_mod('article_text'); ?>;}
-			 nav.menu{ background-color: <?php echo get_theme_mod('menu_background', '#d2bc83'); ?>; }
-			 nav.menu li a{ color: <?php echo get_theme_mod('menu_text', '#000'); ?>; }
-			 nav.menu li a:hover{ background-color: <?php echo get_theme_mod('menu_item_hover', '#2a91d1'); ?>; }
-			 aside li, svg.i-menu {color: <?php echo get_theme_mod('sidebar_text', '#ffffff'); ?>;}
-			 aside a {color: <?php echo get_theme_mod('sidebar_text_link', '#ffffff'); ?>;}
+// Run function (load custom changes) only when customizer is enable.
+if (get_theme_mod('ocean_cream_customizer_enable') == 'enable') {
+    function ocean_cream_customize_css()
+    {
+        $ocean_cream_google_fonts_enable = get_theme_mod('ocean_cream_google_fonts');
+        $ocean_cream_google_fonts_header_enable = get_theme_mod('ocean_cream_google_fonts_header');
+
+        if (!empty($ocean_cream_google_fonts_enable)) {
+            echo '<link href="https://fonts.googleapis.com/css?family=';
+            echo $ocean_cream_google_fonts_enable;
+            echo '" rel="stylesheet">';
+        }
+
+        if (!empty($ocean_cream_google_fonts_header_enable)) {
+            echo '<link href="https://fonts.googleapis.com/css?family=';
+            echo $ocean_cream_google_fonts_header_enable;
+            echo '" rel="stylesheet">';
+        }
+
+        echo '<style>';
+        if (!empty($ocean_cream_google_fonts_enable)) {
+            echo 'article p, .ocean_cream_info, aside li, aside p { font-family:';
+            echo $ocean_cream_google_fonts_enable;
+            echo ',';
+            echo '"Nimbus Sans",Helvetica,Arial,sans}';
+        }
+
+        if (!empty($ocean_cream_google_fonts_header_enable)) {
+            echo 'h1, h2, h3, h4, h5, h6 { font-family:';
+            echo $ocean_cream_google_fonts_header_enable;
+            echo ',';
+            echo '"Nimbus Sans",Helvetica,Arial,sans}';
+        } ?>
+       .ocean_cream_button{ background-color: <?php echo get_theme_mod('ocean_cream_accent_background', '#00b0ff'); ?>!important; }
+			 .ocean_cream_button{ color: <?php echo get_theme_mod('ocean_cream_accent_text', '#3e2723'); ?>; }
+			 .ocean_cream_button:hover{ background-color: <?php echo get_theme_mod('ocean_cream_accent_hover', '#2a91d1'); ?>; }
+			 .page-numbers, .OCnextpage a{ background-color: <?php echo get_theme_mod('ocean_cream_navigation_background', '#00b0ff'); ?>;}
+			 article{ background-color: <?php echo get_theme_mod('ocean_cream_article_background', '#f5eed6'); ?>!important;}
+			 article a{ color: <?php echo get_theme_mod('ocean_cream_article_link'); ?>;}
+			 article p, .ocean_cream_info{ color: <?php echo get_theme_mod('ocean_cream_article_text'); ?>;}
+			 nav.menu, nav ul ul{ background-color: <?php echo get_theme_mod('ocean_cream_menu_background', '#d2bc83'); ?>!important; }
+			 nav.menu li a{ color: <?php echo get_theme_mod('ocean_cream_menu_text', '#000'); ?>; }
+			 nav.menu li a:hover{ background-color: <?php echo get_theme_mod('ocean_cream_menu_item_hover', '#2a91d1'); ?>; }
+			 aside li, aside p, svg.i-menu {color: <?php echo get_theme_mod('ocean_cream_sidebar_text', '#ffffff'); ?>!important;}
+			 aside a {color: <?php echo get_theme_mod('ocean_cream_sidebar_text_link', '#ffffff'); ?>!important;}
          </style>
     <?php
+    }
+    add_action('wp_head', 'ocean_cream_customize_css');
 }
-add_action('wp_head', 'ocean_cream_customize_css');
-?>
