8115fad45f5a6783f9f3d4e9385ddce4bf00ff60
onliniak <34192273+onliniak@users.noreply.github.com>
2019-04-25 15:41:40 +0200

    Travis Code Formatter

--- author.php
+++ author.php
@@ -3,128 +3,131 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 
-    <!-- Mobile zoom -->
-    <meta name=viewport content="width=device-width, initial-scale=1">
-    <link rel="profile" href="http://microformats.org/profile/hcard">
+	<!-- Mobile zoom -->
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://microformats.org/profile/hcard">
 
-    <?php wp_head(); ?>
-    <?php //PHP variables
-    $ocean_cream_website = get_the_author_meta('url', $post->post_author);
-    $ocean_cream_description = get_the_author_meta('description', $post->post_author);
-    $ocean_cream_given_name = get_the_author_meta('first_name', $post->post_author);
-    $ocean_cream_nickname = get_the_author_meta('nickname', $post->post_author);
-    $ocean_cream_family_name = get_the_author_meta('last_name', $post->post_author);
-    ?>
+	<?php wp_head(); ?>
+	<?php //PHP variables
+	$ocean_cream_website = get_the_author_meta('url', $post->post_author);
+	$ocean_cream_description = get_the_author_meta('description', $post->post_author);
+	$ocean_cream_given_name = get_the_author_meta('first_name', $post->post_author);
+	$ocean_cream_nickname = get_the_author_meta('nickname', $post->post_author);
+	$ocean_cream_family_name = get_the_author_meta('last_name', $post->post_author);
+	?>
 </head>
 <body <?php body_class();?>>
-    <!-- Create menu
-    Show on mobile only when click on button -->
-        <div id="ocean_cream_mobile_menu">
-    <?php wp_nav_menu(
-        array(
-            'theme_location' => 'header-menu',
-            'container_class' => 'menu',
-            'container' => 'nav'  )
-        );?>
-        </div>
-        <span id="ocean_cream_mobile_menu_icon">
+	<!-- Create menu
+	Show on mobile only when click on button -->
+		<div id="ocean_cream_mobile_menu">
+	<?php wp_nav_menu(
+		array(
+			'theme_location' => 'header-menu',
+			'container_class' => 'menu',
+			'container' => 'nav'  )
+	);?>
+		</div>
+		<span id="ocean_cream_mobile_menu_icon">
 <svg class="i-menu" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-            <path d="M4 8 L28 8 M4 16 L28 16 M4 24 L28 24"></path>
-        </svg>
-    </span>
+			<path d="M4 8 L28 8 M4 16 L28 16 M4 24 L28 24"></path>
+		</svg>
+	</span>
 
 <main>
 <!-- Widgets, default hide -->
-    <div id="ocean_cream_sidebar">
-    <aside class="ocean_cream_sidebar">
-    <?php dynamic_sidebar('sidebar');?>
-    </aside>
-    </div>
-    <span id="ocean_cream_sidebar_open" class="ocean_cream_button">
+	<div id="ocean_cream_sidebar">
+	<aside class="ocean_cream_sidebar">
+	<?php dynamic_sidebar('sidebar');?>
+	</aside>
+	</div>
+	<span id="ocean_cream_sidebar_open" class="ocean_cream_button">
 		<?php esc_html_e('Show Sidebar', 'ocean-cream'); ?>
 	</span>
 	<!-- Your post. Based on Codex example
 https://developers.google.com/custom-search/docs/structured_data#viewing-extracted-structured-data
 http://microformats.org/wiki/microformats2#authoring-->
 <article <?php post_class('vcard');?> role="main">
-    <h2>
-    <?php esc_html_e('About:', 'ocean-cream');
-        echo '<span class="fn n">';
-    if (!empty($ocean_cream_given_name)) {
-        echo '<span class="given-name">';
-        the_author_meta('first_name');
-        echo "</span> ";
-    }
-    if (!empty($ocean_cream_nickname)) {
-        echo '<span class="nickname">';
-        echo '"';
-        the_author_meta('nickname');
-        echo '"';
-        echo "</span> ";
-    }
-    if (!empty($ocean_cream_family_name)) {
-        echo  '<span class="family-name">';
-        the_author_meta('last_name');
-        echo "</span>";
-        echo "</span>";
-    }
-    ?>
-    </h2>
-    <img style="float:left; margin-right:4px" src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="my avatar" class="h-card photo"/>
-    <dl class="ocean_cream_author_profile">
-        <!-- https://codex.wordpress.org/Template_Tags/the_author_meta -->
-        <dt> <?php esc_html_e('From', 'ocean-cream'); ?>        </dt>
-        <dd> <?php the_author_meta('user_registered'); ?>       </dd>
-    <?php if (!empty($ocean_cream_website)) {
-        echo "<dt>";
-        esc_html_e('Website', 'ocean-cream');
-        echo "</dt>";
-        echo '<dd> <a class="url" href=';
-        the_author_meta('user_url');
-        echo " rel=me>";
-        the_author_meta('user_url');
-        echo "</a>              </dd>";
-    };
-    if (!empty($ocean_cream_description)) {
-        echo "<dt>";
-        esc_html_e('Biography', 'ocean-cream');
-        echo "</dt>";
-        echo '<dd class="note">';
-        the_author_meta('description');
-        echo "</dd>";
-    } ?>
-    </dl>
+	<h2>
+	<?php esc_html_e('About:', 'ocean-cream');
+		echo '<span class="fn n">';
+	if (!empty($ocean_cream_given_name)) {
+		echo '<span class="given-name">';
+		the_author_meta('first_name');
+		echo "</span> ";
+	}
+	if (!empty($ocean_cream_nickname)) {
+		echo '<span class="nickname">';
+		echo '"';
+		the_author_meta('nickname');
+		echo '"';
+		echo "</span> ";
+	}
+	if (!empty($ocean_cream_family_name)) {
+		echo  '<span class="family-name">';
+		the_author_meta('last_name');
+		echo "</span>";
+		echo "</span>";
+	}
+	?>
+	</h2>
+	<img style="float:left; margin-right:4px" src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="my avatar" class="h-card photo"/>
+	<dl class="ocean_cream_author_profile">
+		<!-- https://codex.wordpress.org/Template_Tags/the_author_meta -->
+		<dt> <?php esc_html_e('From', 'ocean-cream'); ?>        </dt>
+		<dd> <?php the_author_meta('user_registered'); ?>       </dd>
+	<?php if (!empty($ocean_cream_website)) {
+		echo "<dt>";
+		esc_html_e('Website', 'ocean-cream');
+		echo "</dt>";
+		echo '<dd> <a class="url" href=';
+		the_author_meta('user_url');
+		echo " rel=me>";
+		the_author_meta('user_url');
+		echo "</a>              </dd>";
+	};
+	if (!empty($ocean_cream_description)) {
+		echo "<dt>";
+		esc_html_e('Biography', 'ocean-cream');
+		echo "</dt>";
+		echo '<dd class="note">';
+		the_author_meta('description');
+		echo "</dd>";
+	} ?>
+	</dl>
 </br>
-    <h2><?php esc_html_e('My posts:', 'ocean-cream'); ?></h2>
+	<h2><?php esc_html_e('My posts:', 'ocean-cream'); ?></h2>
 
-    <ul>
+	<ul>
 <!-- The Loop -->
 
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <li style="list-style:none">
-            <a href="<?php esc_url(the_permalink()) ?>" rel="bookmark">
-            	<?php
-			if (empty($post->post_title)){
-			esc_html_e('Untitled', 'ocean-cream');
-			 }
-		else {
-        the_title();}?></a>,
-            <?php echo get_the_date(); ?>
+	<?php if (have_posts()) :
+		while (have_posts()) :
+			the_post(); ?>
+		<li style="list-style:none">
+			<a href="<?php esc_url(the_permalink()) ?>" rel="bookmark">
+				<?php
+				if (empty($post->post_title)) {
+					esc_html_e('Untitled', 'ocean-cream');
+				} else {
+					the_title();
+				}?></a>,
+			<?php echo get_the_date(); ?>
 			<?php esc_html_e('in', 'ocean-cream'); ?>
 			<?php the_category('&');?>
-        </li>
+		</li>
 
-    <?php endwhile; else: ?>
-        <p><?php esc_html_e('No posts by this author.', 'ocean-cream'); ?></p>
+		<?php endwhile;
+	else : ?>
+		<p><?php esc_html_e('No posts by this author.', 'ocean-cream'); ?></p>
 
-    <?php endif; ?>
+	<?php endif; ?>
 
 <!-- End Loop -->
 
-    </ul>
+	</ul>
 <?php the_posts_pagination(array( 'mid_size' => 2 )); ?>
 </article>
 </main>
-    <?php wp_footer(); ?>
+	<?php wp_footer(); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -2,55 +2,55 @@
 // Add menu in header
 function ocean_cream_menu()
 {
-    $locations = array(
-        'header-menu' => esc_html(__('Header menu', 'ocean-cream'))
-    );
-    register_nav_menus($locations);
+	$locations = array(
+		'header-menu' => esc_html(__('Header menu', 'ocean-cream'))
+	);
+	register_nav_menus($locations);
 }
 add_action('init', 'ocean_cream_menu');
 // Add sidebar support
 add_action('widgets_init', 'ocean_cream_widgets');
 function ocean_cream_widgets()
 {
-    register_sidebar(
-        array(
-        'name' => esc_html(__('Widget Area', 'ocean-cream')),
-        'id' => 'sidebar',
-        'description' => esc_html(__('Add widgets here to appear in your sidebar.', 'ocean-cream'))
-        )
-    );
+	register_sidebar(
+		array(
+		'name' => esc_html(__('Widget Area', 'ocean-cream')),
+		'id' => 'sidebar',
+		'description' => esc_html(__('Add widgets here to appear in your sidebar.', 'ocean-cream'))
+		)
+	);
 }
 // Register Theme Features
 function ocean_cream_features()
 {
-    add_editor_style('/css/editor-style.css');
-    set_post_thumbnail_size(600, 600, true);
+	add_editor_style('/css/editor-style.css');
+	set_post_thumbnail_size(600, 600, true);
 
-    // HTML5 and RSS
-    add_theme_support('automatic-feed-links');
-    add_theme_support(
-        'html5',
-        array(
-        'comment-list',
-        'comment-form',
-        'search-form',
-        'gallery',
-        'caption'
-        )
-    );
-    // Title tag and translations
-    load_theme_textdomain('ocean-cream', get_template_directory() . '/languages');
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
+	// HTML5 and RSS
+	add_theme_support('automatic-feed-links');
+	add_theme_support(
+		'html5',
+		array(
+		'comment-list',
+		'comment-form',
+		'search-form',
+		'gallery',
+		'caption'
+		)
+	);
+	// Title tag and translations
+	load_theme_textdomain('ocean-cream', get_template_directory() . '/languages');
+	add_theme_support('title-tag');
+	add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'ocean_cream_features');
 if (!isset($content_width)) {
-    $content_width = 600;
+	$content_width = 600;
 }
 //add more link to excerpt
 function ocean_cream_excerpt($more)
 {
-    return ' <a class="read-more" href="' . esc_url(get_permalink(get_the_ID())) . '">' . esc_html(__('Read More', 'ocean-cream')) . '</a>';
+	return ' <a class="read-more" href="' . esc_url(get_permalink(get_the_ID())) . '">' . esc_html(__('Read More', 'ocean-cream')) . '</a>';
 }
 add_filter('excerpt_more', 'ocean_cream_excerpt');
 
@@ -58,13 +58,13 @@ add_filter('excerpt_more', 'ocean_cream_excerpt');
 add_action('wp_head', 'ocean_cream_header', 1);
 function ocean_cream_header()
 {
-    wp_enqueue_style('ocean_cream_criticalcss', get_template_directory_uri() . '/css/critical.min.css', array(), '20190103', 'screen');
+	wp_enqueue_style('ocean_cream_criticalcss', get_template_directory_uri() . '/css/critical.min.css', array(), '20190103', 'screen');
 }
 // Load other scripts and styles (after load site).
 function ocean_cream_footer()
 {
-    wp_enqueue_style('ocean_cream_maincss', get_template_directory_uri() . '/css/style.min.css', array(), '20190417', 'screen');
-    wp_enqueue_script('ocean_cream_buttons', get_template_directory_uri() . '/js/buttons.min.js');
-    wp_enqueue_style('ocean_cream_printcss', get_template_directory_uri() . '/css/print.css', array(), '20181206', 'print');
+	wp_enqueue_style('ocean_cream_maincss', get_template_directory_uri() . '/css/style.min.css', array(), '20190417', 'screen');
+	wp_enqueue_script('ocean_cream_buttons', get_template_directory_uri() . '/js/buttons.min.js');
+	wp_enqueue_style('ocean_cream_printcss', get_template_directory_uri() . '/css/print.css', array(), '20181206', 'print');
 }
 add_action('wp_footer', 'ocean_cream_footer');
